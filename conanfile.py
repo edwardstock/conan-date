ec65aec500d1530345734a54ab5e42de2fcceda7
Uilian Ries <uilianries@gmail.com>
2018-09-04 19:17:49 -0300

    Build using Bincrafters templates
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- conanfile.py
+++ conanfile.py
@@ -5,80 +5,53 @@ from conans import ConanFile, CMake, tools
 import os
 
 
-class LibnameConan(ConanFile):
+class DateConan(ConanFile):
     name = "date"
     version = "2.4.1"
     url = "https://github.com/bincrafters/conan-date"
-    description = "A date and time library based on the C++11/14/17 <chrono> header "
-
-    # Indicates License type of the packaged library
+    homepage = "https://github.com/HowardHinnant/date"
+    author = "Bincrafters <bincrafters@gmail.com>"
+    description = "A date and time library based on the C++11/14/17 <chrono> header"
     license = "MIT"
-
-    # Packages the license for the conanfile.py
     exports = ["LICENSE.md"]
-
-    # Remove following lines if the target lib does not use cmake.
     exports_sources = ["CMakeLists.txt"]
     generators = "cmake"
-
-    # Options may need to change depending on the packaged library.
     settings = "os", "arch", "compiler", "build_type"
-    options = {"shared": [True, False], "use_system_tz_db": [True, False],
-               "use_tz_db_in_dot": [True, False], }
-    default_options = ("shared=False", "use_system_tz_db=True", "use_tz_db_in_dot=False")
-
-    # Custom attributes for Bincrafters recipe conventions
+    options = {"shared": [True, False], "fPIC": [True, False], "use_system_tz_db": [True, False], "use_tz_db_in_dot": [True, False]}
+    default_options = ("shared=False", "fPIC=True", "use_system_tz_db=True", "use_tz_db_in_dot=False")
     source_subfolder = "source_folder"
     build_folder = "build_folder"
 
-    # Use version ranges for dependencies unless there's a reason not to
+    def config_options(self):
+        if self.settings.os == "Windows":
+            self.options.remove("fPIC")
+
     def requirements(self):
         if not self.options.use_system_tz_db:
             self.requires("libcurl/[>=7.52.1]@bincrafters/stable")
 
     def source(self):
-        source_url = "https://github.com/HowardHinnant/date"
-        tools.get("{0}/archive/v{1}.tar.gz".format(source_url, self.version))
+        tools.get("{0}/archive/v{1}.tar.gz".format(self.homepage, self.version))
         extracted_dir = self.name + "-" + self.version
-
-        #Rename to "source_subfolder" is a convention to simplify later steps
         os.rename(extracted_dir, self.source_subfolder)
 
-        # Helper method for common CMake configurations
-
-        self.wrap_cmake()
-
-    def build(self):
+    def configure_cmake(self):
         cmake = CMake(self)
-        cmake.definitions["BUILD_TESTS"] = False # example
+        cmake.definitions["ENABLE_DATE_TESTING"] = False
         cmake.definitions["USE_SYSTEM_TZ_DB"] = self.options.use_system_tz_db
         cmake.definitions["USE_TZ_DB_IN_DOT"] = self.options.use_tz_db_in_dot
         cmake.definitions["BUILD_TZ_STATIC"] = not self.options.shared
-        cmake.configure(source_folder=self.source_subfolder, build_folder=self.build_folder)
+        cmake.configure(build_folder=self.build_folder)
+        return cmake
+
+    def build(self):
+        cmake = self.configure_cmake()
         cmake.build()
-        cmake.install()
 
     def package(self):
-        # If the CMakeLists.txt has a proper install method, the steps below may be redundant
-        # If so, you can replace all the steps below with the word "pass"
-        include_folder = os.path.join(self.source_subfolder, "include")
-        self.copy(pattern="LICENSE")
-        self.copy(pattern="*", dst="include", src=include_folder)
-        self.copy(pattern="*.dll", dst="bin", keep_path=False)
-        self.copy(pattern="*.lib", dst="lib", keep_path=False)
-        self.copy(pattern="*.a", dst="lib", keep_path=False)
-        self.copy(pattern="*.so*", dst="lib", keep_path=False)
-        self.copy(pattern="*.dylib", dst="lib", keep_path=False)
-
+        self.copy(pattern="LICENSE", dst="licenses", src=self.source_subfolder)
+        cmake = self.configure_cmake()
+        cmake.install()
 
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
-
-
-    # Helper method for common CMake configurations
-    def wrap_cmake(self):
-        with tools.chdir(self.source_subfolder):
-            os.rename("CMakeLists.txt", "CMakeListsOriginal.txt")
-
-        cmake_wrapper_new = os.path.join(self.source_subfolder, "CMakeLists.txt")
-        os.rename("CMakeLists.txt", cmake_wrapper_new)
