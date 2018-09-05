fa69a13c5cf7f8dd3c8d9a7981b722d2c84e28a6
Uilian Ries <uilianries@gmail.com>
2018-09-04 22:20:31 -0300

    Copy UTC file
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- build.py
+++ build.py
@@ -1,19 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-
 from bincrafters import build_template_default
-from conans import tools
 
 
 if __name__ == "__main__":
     builder = build_template_default.get_builder(pure_c=False)
-
-    if tools.os_info.is_windows:
-        filtered_builds = []
-        for settings, options, env_vars, build_requires, reference in builder.items:
-            if "MT" not in settings["compiler.runtime"]:
-                filtered_builds.append([settings, options, env_vars, build_requires])
-        builder.builds = filtered_builds
-
     builder.run()
--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
 
-from conans import ConanFile, CMake
+from conans import ConanFile, CMake, tools
 import os
 
 
@@ -10,12 +10,23 @@ class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake"
 
+    def copy_utc(self):
+        if tools.os_info.is_linux:
+            zoneinfo_dir = os.path.join(os.sep, "usr", "share", "zoneinfo")
+            try:
+                if not os.path.exists(zoneinfo_dir):
+                    self.run("sudo mkdir {}".format(zoneinfo_dir))
+                self.run("sudo cp UTC {}".format(os.path.join(zoneinfo_dir, "UTC")))
+            except:
+                pass
+
     def build(self):
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
+        self.copy_utc()
 
     def test(self):
-        os.environ["TZ"] = "America/Los_Angeles"
-        bin_path = os.path.join("bin", "test_package")
-        self.run(bin_path, run_environment=True)
+        with tools.environment_append({"TZ": "America/Los_Angeles"}):
+            bin_path = os.path.join("bin", "test_package")
+            self.run(bin_path, run_environment=True)
