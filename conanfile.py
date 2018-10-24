a2a320be181aa256d09199eebd389e81f40a2c18
Croydon <cr0ydon@outlook.com>
2018-10-24 22:11:58 +0200

    Update with latest templates

--- conanfile.py
+++ conanfile.py
@@ -8,19 +8,19 @@ import os
 class DateConan(ConanFile):
     name = "date"
     version = "2.4.1"
+    description = "A date and time library based on the C++11/14/17 <chrono> header"
     url = "https://github.com/bincrafters/conan-date"
     homepage = "https://github.com/HowardHinnant/date"
     author = "Bincrafters <bincrafters@gmail.com>"
-    description = "A date and time library based on the C++11/14/17 <chrono> header"
     license = "MIT"
     exports = ["LICENSE.md"]
     exports_sources = ["CMakeLists.txt"]
     generators = "cmake"
     settings = "os", "arch", "compiler", "build_type"
     options = {"shared": [True, False], "fPIC": [True, False], "use_system_tz_db": [True, False], "use_tz_db_in_dot": [True, False]}
-    default_options = ("shared=False", "fPIC=True", "use_system_tz_db=True", "use_tz_db_in_dot=False")
-    source_subfolder = "source_folder"
-    build_folder = "build_folder"
+    default_options = {"shared": False, "fPIC": True, "use_system_tz_db": True, "use_tz_db_in_dot": False}
+    _source_subfolder = "source_subfolder"
+    _build_subfolder = "build_subfolder"
 
     def config_options(self):
         if self.settings.os == "Windows":
@@ -36,25 +36,25 @@ class DateConan(ConanFile):
             self.requires("libcurl/7.56.1@bincrafters/stable")
 
     def source(self):
-        tools.get("{0}/archive/v{1}.tar.gz".format(self.homepage, self.version))
+        tools.get("{0}/archive/v{1}.tar.gz".format(self.homepage, self.version), sha256="98907d243397483bd7ad889bf6c66746db0d7d2a39cc9aacc041834c40b65b98")
         extracted_dir = self.name + "-" + self.version
-        os.rename(extracted_dir, self.source_subfolder)
+        os.rename(extracted_dir, self._source_subfolder)
 
-    def configure_cmake(self):
+    def _configure_cmake(self):
         cmake = CMake(self)
         cmake.definitions["ENABLE_DATE_TESTING"] = False
         cmake.definitions["USE_SYSTEM_TZ_DB"] = self.options.use_system_tz_db
         cmake.definitions["USE_TZ_DB_IN_DOT"] = self.options.use_tz_db_in_dot
-        cmake.configure(build_folder=self.build_folder)
+        cmake.configure(build_folder=self._build_subfolder)
         return cmake
 
     def build(self):
-        cmake = self.configure_cmake()
+        cmake = self._configure_cmake()
         cmake.build()
 
     def package(self):
-        self.copy(pattern="LICENSE.txt", dst="licenses", src=self.source_subfolder)
-        cmake = self.configure_cmake()
+        self.copy(pattern="LICENSE.txt", dst="licenses", src=self._source_subfolder)
+        cmake = self._configure_cmake()
         cmake.install()
 
     def package_info(self):
